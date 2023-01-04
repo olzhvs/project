5191b22871ab236a05d0342df74317de5e9fd2ae
Olzhas <olzhas.mp@gmail.com>
2023-01-04 22:36:49 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,142 @@
+// // // console.log(1);
+
+"use strict";
+// // let a=15;
+
+// // let number = 5;
+// // const leftBorderWidth = 1;
+
+// // number = 10;
+
+// // console.log(number);
+
+// // const obj = {
+// //   a: 50,
+// // };
+
+// // obj.a = 10;
+
+// // console.log(obj);
+
+// // var name = 'Ivan';
+
+// // const vehicleBodyWidth = 5000;
+// // const vehicleBodyLength = 4000;
+
+// // console.log(
+// //   "Ширина кузова авто: " + vehicleBodyWidth + ",  длина: " + vehicleBodyLength
+// // );
+
+// // ['vruru', 'ecer', 'ececec'].map(a =>);
+
+// //request //
+
+// // snake_case//
+// // UPPER_SNAKE_CASE//
+
+// // let COLOR_RED = "F00"; //нельзя переопределять
+
+// // // Kebab-case-
+// // //PascalCase
+
+// // let number = 4.6;
+
+// // console.log(-4/0);
+// // console.log('string' * 9);
+
+// // const person = 'Alex';
+
+// // const bool = false;
+
+// // console.log(something);
+
+// // let und;
+
+// // console.log(und);
+
+// // const obj = {
+// //   name:"John",
+// //   age: 25,
+// //   isMarried: false
+// // };
+
+// // console.log(obj.age);
+
+// // let arr=['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// // console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj={
+//   0:'a',
+//   1:'b',
+//   2:'c',
+//   abc: {
+//     df:[{}, {}],
+//     def:{
+
+//     }
+//   }
+// };
+
+// const b='b';
+// arrObj[b] = '1234';
+
+// console.log(arrObj['b']);
+
+// console.log(arr[1]);
+// const obj = {
+//   // a: 1,
+//   // b: 2,
+//   Anna: 500,
+//   'Alice': 800
+// };
+//  console.log(obj.Alice);
+
+// alert("Hello World");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18y.o?", "Yes", " No");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Your name?", "");
+// answers[1] = prompt("Your last name?", "");
+// answers[2] = prompt("Your age?", "");
+
+// // document.write(answers);
+// console.log(typeof answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// console.log("arr" + " - object");
+// console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === "8");
+
+const isChecked = false,
+  isClose = false;
+
+console.log(isChecked || !isClose);
