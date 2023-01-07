a5747293c6ebfaf8d0d095f5f3b44898afb505f6
Olzhas <olzhas.mp@gmail.com>
2023-01-07 21:17:06 +0600

    js prilozhenie

--- js/script.js
+++ js/script.js
@@ -140,3 +140,22 @@
 //   isClose = false;
 
 // console.log(isChecked || !isClose);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из просмотренных фильмов", ""),
+  b = prompt("На сколько оцение?", ""),
+  c = prompt("Один из просмотренных фильмов", ""),
+  d = prompt("На сколько оцение?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
